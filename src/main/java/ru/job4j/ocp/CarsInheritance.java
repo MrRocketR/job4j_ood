746e93e57b53400564784db18ebf1c671bdb480c
MrRocketR <kuptovns@gmail.com>
2022-07-24 20:09:15 +0300

    0. Принцип открытости закрытости

---
+++ src/main/java/ru/job4j/ocp/CarsInheritance.java
@@ -0,0 +1,26 @@
+package ru.job4j.ocp;
+
+import java.util.List;
+
+import java.util.List;
+
+public class CarsInheritance {
+
+    private static class Car {
+        public String sound() {
+            return "beep-beep";
+        }
+    }
+
+    private static class NoSoundCar extends Car {
+        @Override
+        public String sound() {
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Car> cars = List.of(new Car(), new Car(), new NoSoundCar());
+        cars.forEach(Car::sound);
+    }
+}
